3c38780894bd727e22eb3fbf1f0b2972745b70d5
Jacob True <147014343+jfalse@users.noreply.github.com>
2024-07-19 01:07:33 -0400

    Update first_script.sh
    added args
    
    -checks if arg exists
    -checks if arg is it exists, & is a directory or file.

--- first_script.sh
+++ first_script.sh
@@ -1,10 +1,21 @@
+sysop@sysadmin03:~/assignments/scripting$ cat ./simple_shell.sh
 #!/bin/bash
 
 fn=$(ls -lh | awk '{print $9}')
 
+if [ -n "$1" ]; then
+        printf "\nArgs exist checking Args for filetype...\n---------------------------------------------------\n"
+        if [ -d "$1" ]; then
+                printf "%s is a directory\n\n" "$1"
+        elif [ -f "$1" ]; then
+                printf "%s is not a directory it is a Regular File\n\n" "$1"
+        else
+                printf "%s directory does not exist\n\n" "$1"
+        fi
+fi
+
 printf "%-20s %-10s %-15s\n" "Filename" "lines" "Type"
 printf "%-20s %-10s %-15s\n" "----------" "------" "-----------"
-
 for file in $fn;
 do
         if [ -f "$file" ]; then
